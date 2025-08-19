623ee9dba33b6559c5cec98f3be85e31ab6df9d1
Brryan12 <Bryancr1208@gmail.com>
2025-08-18 19:33:20 -0600

    desarrollo de la ventana inicio

--- src/main/java/com/example/hotelmanager/InicioController.java
+++ src/main/java/com/example/hotelmanager/InicioController.java
@@ -1,6 +1,36 @@
 package com.example.hotelmanager;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import javafx.application.Platform;
+
 public class InicioController {
+
+    @FXML private TextField txtBuscarCli;
+    @FXML private Button btnBuscarCli;
+    @FXML private Button btnModificarCli;
+    @FXML private Button btnEliminarCli;
+    @FXML private Button btnAgregarCli;
+
+    @FXML private String BuscarCli(){
+    /*
+    if(nameTabla == txtBuscarCli.getText())
+        muestra solamente el cliente
+    else
+         tira error que le usuario que busco no se encuentra en la tabla
+     */
+        return txtBuscarCli.getText();
+    }
+    @FXML private void btnModificarCliOnAction(ActionEvent event) {}
+    @FXML private void btnEliminarCliOnAction(ActionEvent event) {}
+    @FXML private void btnAgregarCliOnAction(ActionEvent event) {}
+
+
 
 
 }
