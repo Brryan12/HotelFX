13a0de39cf3a7202b0909838895ace4f8ac8d673
Brryan12 <bryancr1208@gmail.com>
2025-09-06 19:23:26 -0600

    intento 2

---
+++ src/main/java/com/example/hotelmanager/datos/ClienteEntity.java
@@ -0,0 +1,67 @@
+package com.example.hotelmanager.datos;
+import jakarta.xml.bind.annotation.*;
+import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.time.LocalDate;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ClienteEntity {
+    private int id;
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String identificacion;
+
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
+    private LocalDate fechaNacimiento;
+
+    public ClienteEntity() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,12 +2,18 @@ module com.example.hotelmanager {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.base;
+    requires jakarta.xml.bind;
 
-
+    // Paquete raíz
     opens com.example.hotelmanager to javafx.fxml;
     exports com.example.hotelmanager;
+
+    // Aplicación
     exports com.example.hotelmanager.model;
     opens com.example.hotelmanager.model to javafx.fxml;
+
+    opens com.example.hotelmanager.datos to jakarta.xml.bind;
+    exports com.example.hotelmanager.datos;
     exports com.example.hotelmanager.controller;
     opens com.example.hotelmanager.controller to javafx.fxml;
 }
